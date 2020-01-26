e32e9f2d9e0b49dddae5308d120b7f20d6218c7d
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2020-01-26 19:30:19 +0800

    feat: finish the exercise part 1

--- app/src/main/java/com/example/baseemptytemplate/MainActivity.kt
+++ app/src/main/java/com/example/baseemptytemplate/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.baseemptytemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var mCount: Int = 0
+        private
+
+    lateinit var mShowCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: View) {
+        val toast: Toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+    fun countUp(view: View) {
+        mCount++
+        if (mShowCount !== null)
+            mShowCount.text = mCount.toString()
     }
 }
